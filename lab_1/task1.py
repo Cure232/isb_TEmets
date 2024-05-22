d83dee761b1523413c6c0e91a4481203e168a011
Cure232 <timtim2004232@gmail.com>
2024-05-22 23:55:36 +0400

    .json creation relocated

--- lab_1/task1.py
+++ lab_1/task1.py
@@ -2,6 +2,19 @@ import json
 from random import randint
 
 
+def read_json(file_path: str) -> dict:
+    """function, which can get dict from .json file
+
+    Returns:
+        dict[str:str]: dictionary with pair (key - value)
+    """
+    try:
+        with open(file_path, 'r', encoding="UTF-8") as file:
+            return json.load(file)
+    except Exception as e:
+       print("Произошла ошибка:", e)
+
+
 def write_json(file_path: str, content: dict) -> None:
     """function, that writes data to .json file
 
@@ -16,26 +29,47 @@ def write_json(file_path: str, content: dict) -> None:
        print("Произошла ошибка:", e)
 
 
+def set_new_cypher_parameters(path: str, new_alphabet: dict | None = None, new_key: str | None = None) -> None:
+    """function that sets new alphabet and/or key
+       to existing .json key file or creates new one with given data
+
+    Args:
+        path (str): path to .json file or new file path.
+        new_alphabet (dict | None, optional): preferred new alphabet. Defaults to None.
+        new_key (str | None, optional): preferred new key. Defaults to None.
+    """
+    json_content = dict()
+    try:
+        json_content = read_json(path)
+    except Exception as e:
+        print(e, "\nФайл не найден, будет создан новый.")
+
+    if new_alphabet:
+        json_content["alphabet"] = dict()
+        for letter in new_alphabet:
+            letter = str(letter).lower()
+            while True:
+                x = randint(1, len(new_alphabet.keys()))
+                if x not in list(json_content['alphabet'].values()):
+                    json_content['alphabet'][letter] = x
+                    break
+    
+    if new_key:
+        json_content["key"] = new_key.lower()
+        numerise_letter = lambda letter: json_content['alphabet'][letter]
+        json_content['key_numerised_value'] = [numerise_letter() for letter in json_content['key']]
+
+    write_json(path, json_content)
+
+
 def main() -> None:
     russian_alphabet = ['а', 'б', 'в', 'г', 'д', 'е', 'ё',
                          'ж', 'з', 'и', 'й', 'к', 'л', 'м',
                            'н', 'о', 'п', 'р', 'с', 'т', 'у',
                              'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ',
                                'ы', 'ь', 'э', 'ю', 'я']
-    json_content = dict()
-    json_content["alphabet"] = dict()
+    set_new_cypher_parameters("task1/key.json", russian_alphabet, "панграмма")
 
-    for letter in russian_alphabet:
-        while True:
-            x = randint(1,33)
-            if x not in list(json_content['alphabet'].values()):
-                json_content['alphabet'][letter] = x
-                break
-    
-    json_content["key"] = "панграмма"
-    numerise_letter = lambda letter: json_content['alphabet'][letter]
-    json_content['key_numerised_value'] = [numerise_letter(letter) for letter in json_content['key']]
-    write_json("task1/key.json",json_content)
 
 
 if __name__ == '__main__':
